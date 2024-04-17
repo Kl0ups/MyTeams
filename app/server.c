4fdb086b7df873843447858b4fa2f60f9d821006
Yanis Moyon <yanis.moyon@epitech.eu>
2024-04-17 18:05:36 +0200

    ADD server arg parsing

--- app/server.c
+++ app/server.c
@@ -4,8 +4,23 @@
 ** File description:
 ** server.c
 */
+#include "server/data_utils.h"
+#include "server/launch_utils.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(void)
+int main(int ac, const char *av[])
 {
-    return (0);
+    int port = -1;
+
+    if (ac != 2) {
+        write(2, "Invalid number of arguments\n", 29);
+        return (84);
+    }
+    port = (int)strtol(av[1], NULL, 10);
+    if (port < 0) {
+        write(2, "Invalid port\n", 13);
+        return (84);
+    }
+    return (teams_server(port));
 }
