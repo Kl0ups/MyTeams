dd124da8d99e282d35df52e9287aa3360cd11cf6
Yanis Moyon <yanis.moyon@epitech.eu>
2024-04-17 18:04:27 +0200

    FIX add documentation & add owner id to thread_t

--- include/common_utils.h
+++ include/common_utils.h
@@ -11,6 +11,17 @@
 #include <uuid/uuid.h>
 #include <stdbool.h>
 
+/// @brief Structure containing all the data of a client
+/// @param id The unique identifier of the client
+/// @param fd The file descriptor of the client
+/// @param username The username of the client
+/// @param password The password of the client
+/// @param t_msg The temporary sending message of the client
+/// @param r_msg The temporary received message of the client
+/// @param is_active The activity status of the client
+/// @param team The selected team of the client (NULL if none selected)
+/// @param channel The selected channel of the client (NULL if none selected)
+/// @param thread The selected thread of the client (NULL if none selected)
 typedef struct client_s {
     uuid_t id;
     int fd;
@@ -20,38 +31,72 @@ typedef struct client_s {
     uuid_t *team, *channel, *thread;
 } client_t;
 
+/// @brief Structure containing all the data of a team
+/// @param id The unique identifier of the team
+/// @param ctt The creation time of the team
+/// @param name The name of the team
+/// @param description The description of the team
+/// @param channels The list of channels id of the team
+/// @param members The list of members id of the team
 typedef struct team_s {
     uuid_t id;
     time_t ctt;
-    char *name, *description;
-    uuid_t *channels;
-    uuid_t *members;
+    char *name, *description; // name and description
+    uuid_t *channels; // list of channels (channel_t)
+    uuid_t *members; // list of members (client_t)
 } team_t;
 
+/// @brief Structure containing all the data of a channel
+/// @param id The unique identifier of the channel
+/// @param ctt The creation time of the channel
+/// @param name The name of the channel
+/// @param description The description of the channel
+/// @param threads The list of threads id of the channel
+/// @param members The list of members id of the channel
 typedef struct channel_s {
     uuid_t id;
     time_t ctt;
-    char *name, *description;
-    uuid_t *threads;
-    uuid_t *members;
+    char *name, *description; // name and description
+    uuid_t *threads; // list of threads (thread_t)
+    uuid_t *members; // list of members (client_t)
 } channel_t;
 
+/// @brief Structure containing all the data of a thread
+/// @param id The unique identifier of the thread
+/// @param ctt The creation time of the thread
+/// @param owner The owner of the thread (client_t)
+/// @param name The title of the thread
+/// @param post The body of the thread
+/// @param r_message The list of replies id of the thread
 typedef struct thread_s {
     uuid_t id;
     time_t ctt;
-    char *name;
-    char *post;
-    uuid_t *r_message;
+    uuid_t owner;
+    char *name; // title
+    char *post; // body
+    uuid_t *r_message; // list of replies (message_t)
 } thread_t;
 
+/// @brief Structure containing all the data of a message
+/// @param id The unique identifier of the message
+/// @param ctt The creation time of the message
+/// @param t_client The sender client of the message (client_t)
+/// @param r_client The receiver client/post of the message (client_t/thread_t)
+/// @param msg The body of the message
 typedef struct message_s {
     uuid_t id;
-    time_t ctt;
-    uuid_t t_client;
-    uuid_t r_client;
+    time_t ctt; // creation time
+    uuid_t t_client; // sender client (client_t)
+    uuid_t r_client; // receiver client (client_t)
     char *msg;
 } message_t;
 
+/// @brief Union containing all the data structures
+/// @param client The client structure
+/// @param team The team structure
+/// @param channel The channel structure
+/// @param thread The thread structure
+/// @param message The message structure
 typedef union data_u {
     client_t client;
     team_t team;
@@ -60,6 +105,11 @@ typedef union data_u {
     message_t message;
 } data_t;
 
+/// @brief Structure containing the data to transfer
+/// @param t_command The command to execute
+/// @param p_size The size of the data to transfer
+/// @param p_data The data to transfer (list or unary element(s))
+/// @param p_extra The extra data to transfer, if needed
 typedef struct transfer_s {
     char *t_command;
     unsigned int p_size;
