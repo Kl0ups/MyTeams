29ce6a5c5a8a73dcfa7ae052bf768b5a59ec996e
Yanis Moyon <yanis.moyon@epitech.eu>
2024-04-18 19:21:38 +0200

    ADD completed server loop

--- src/server/server_main_functions.c
+++ src/server/server_main_functions.c
@@ -9,31 +9,15 @@
 #include "server/database_utils.h"
 #include "server/launch_utils.h"
 #include "server/data_freers.h"
+#include "server/serverset_managers.h"
+#include "server/logging_server.h"
 
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdio.h>
 
-// static int handle_read_set(server_t *serv)
-// {
-//     int ret = 0;
-
-//     if (FD_ISSET(serv->fd, serv->r_set)) {
-//         add_client(serv);
-//         return 0;
-//     }
-//     ret = handle_clients(serv);
-//     if (ret == SYSTEM_ERROR && close(serv->fd)) {
-//         return -1;
-//     }
-//     print_clients(serv, ret);
-//     return 0;
-// }
-
-// static int handle_write_set(server_t *serv);
-
-void server_free_data(server_t *serv)
+static void server_free_data(server_t *serv)
 {
     if (serv->clients)
         server_clear_clients(serv);
@@ -54,27 +38,22 @@ void server_free_data(server_t *serv)
     free(serv);
 }
 
-// TODO: fix this function
-// int max_fd = 0;
-// int read = 0;
-// int write = 0;
-
-// while (1) {
-//     clear_serverset(*serv);
-//     max_fd = setup_serverset(*serv);
-//     if (server_select(*serv, max_fd)) {
-//         close(serv->fd);
-//         return 84;
-//     }
-//     read = handle_read_set(serv);
-//     write = handle_write_set(serv);
-//     if (write < 0 || read < 0) {
-//         return 84;
-//     }
-// }
 static int server_loop(server_t *serv)
 {
-    server_free_data(serv);
+    int max_fd = 0;
+    int read = 0;
+    int write = 0;
+
+    while (1) {
+        clear_serverset(*serv);
+        max_fd = setup_serverset(*serv);
+        if (server_select(*serv, max_fd))
+            return 1;
+        read = manage_read_set(serv);
+        write = manage_write_set(serv);
+        if (write < 0 || read < 0)
+            return 1;
+    }
     return 0;
 }
 
@@ -82,25 +61,23 @@ static server_t *start_server(server_t *inst, int port)
 {
     int db = check_database();
 
-    if (port < 0 || !inst) {
+    if (port < 0 || !inst)
         return NULL;
-    }
-    if (!init_server(inst, port)) {
+    if (!init_server(inst, port))
         return NULL;
-    }
-    if (load_database(inst, db) == -1) {
+    if (load_database(inst, db) == -1)
         return NULL;
-    }
     return inst;
 }
 
 int teams_server(int port)
 {
+    int ret = 0;
     server_t *serv = malloc(sizeof(server_t));
 
-    if (!start_server(serv, port)) {
-        fprintf(stderr, "Error while starting server\n");
+    if (!start_server(serv, port))
         return 84;
-    }
-    return server_loop(serv);
+    ret = server_loop(serv);
+    server_free_data(serv);
+    return ret ? 84 : 0;
 }
