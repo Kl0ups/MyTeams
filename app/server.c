3eb63714d33feace987c71ad596cf4bf357dd3f7
Yanis Moyon <113261583+Kl0ups@users.noreply.github.com>
2024-04-19 13:25:54 +0200

    Revert "ADD Server loop and Database loading"

--- app/server.c
+++ app/server.c
@@ -4,19 +4,8 @@
 ** File description:
 ** server.c
 */
-#include "server/data_utils.h"
-#include "server/launch_utils.h"
-#include <stdlib.h>
-#include <unistd.h>
 
-int main(int ac, const char *av[])
+int main(void)
 {
-    int port = -1;
-
-    if (ac != 2)
-        return (84);
-    port = (int)strtol(av[1], NULL, 10);
-    if (port < 0)
-        return (84);
-    return (teams_server(port));
+    return (0);
 }
